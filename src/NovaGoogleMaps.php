175366c02906c9e1652085ae620dd62a00b0291b
jfeid <giannisfd@gmail.com>
2018-11-27 15:59:41 +0200

    Initial commit

---
+++ src/NovaGoogleMaps.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Jfeid\NovaGoogleMaps;
+
+use Laravel\Nova\Fields\Field;
+
+class NovaGoogleMaps extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'nova-google-maps';
+}
